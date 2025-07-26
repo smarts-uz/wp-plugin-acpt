7d488b87087c3984962ac207426d597f49298125
SaidovAliakbar <developerweb250@gmail.com>
2025-07-26 12:33:38 +0500

    Used get_acpt_meta_group_object()

--- wp-content/themes/storefront/content-single.php
+++ wp-content/themes/storefront/content-single.php
@@ -46,7 +46,41 @@
 	// assoc_acpt_taxonomy_to_acpt_post('ctax', 'post');
 
 	//remove_assoc_acpt_taxonomy_from_acpt_post
-	remove_assoc_acpt_taxonomy_from_acpt_post('ctax', 'post');
+	// remove_assoc_acpt_taxonomy_from_acpt_post('ctax', 'post');
+
+	//save_acpt_form
+	// save_acpt_form([
+	// 	'name' => 'add-cpt',
+	// 	'label' => 'Add CPT',
+	// 	'key' => '96b3303d',
+	// 	'action' => 'Post management',
+	// 	'fields' => [],
+	// 	'meta' => []
+	// ]);
+
+	//save_acpt_form_field
+	// echo save_acpt_form_field([
+	// 	'form_name' => 'add-cpt',
+	// 	'key' => '',
+	// 	'group' => 'group-first',
+	// 	'name' => 'cpt-fields-time',
+	// 	'label' => 'cpt-fields-time',
+	// 	'type' => 'Time',
+	// 	'description' => 'description',
+	// 	'isRequired' => false,
+	// 	'extra' => [],
+	// 	'settings' => [],
+	//  ]);
+
+	//get_acpt_meta_group_objects
+	echo '<pre>';
+	print_r (get_acpt_meta_group_objects());
+	echo '</pre>';
+
+	//get_acpt_meta_group_object
+	echo '<pre>';
+	print_r (get_acpt_meta_group_object('group-first'));
+	echo '</pre>';
 
 	/**
 	 * Functions hooked in to storefront_single_post_bottom action
